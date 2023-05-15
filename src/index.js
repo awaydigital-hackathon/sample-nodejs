ca646dd95c60c50735516255d068d8a13a8a571b
tamawaydigitalgroup1 <132558345+tamawaydigitalgroup1@users.noreply.github.com>
2023-05-15 06:19:57 +0000

    update

--- src/index.js
+++ src/index.js
@@ -5,3 +5,18 @@ let b = "1";
 if(a==b) {
     console.log('')
 }
+
+function HelloWorld(hour) {
+    if (hour) {
+      this.hour = hour;
+    } else {
+      var date = new Date();
+      this.hour = date.getHours();
+    }
+    this.displayGreeting = function() {
+      if (this.hour >= 22 || this.hour <= 5)
+        document.write("Good night, World!");
+      else
+        document.write("Hello, World!");
+    } 
+  }
